5a4b99c715bcada6a158dbddac264f01a45562d6
David <davood.frontend@gmail.com>
2024-08-13 21:15:08 +0330

    feat: finish the design of action part

--- app/components/main/currentAlbum/Songs.tsx
+++ app/components/main/currentAlbum/Songs.tsx
@@ -2,18 +2,17 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 
 import Song from './Song';
-const myAray = new Array(120).fill('lol')
+const myAray = new Array(120).fill('lolita')
 const Songs = () => {
 
 
     return (
-        <Box sx={{ px: 3, pt: 2, pb: 1, flexGrow: 1, flexBasis: 0, display: 'flex', flexDirection: 'column' }}>
+        <Box sx={{ px: 2, pt: 2, pb: 1, flexGrow: 1, flexBasis: 0, display: 'flex', flexDirection: 'column' }}>
             <Typography>
                 Your Songs
             </Typography>
             <Box sx={{ overflow: 'auto', flexGrow: 1, flexBasis: 0, mt: 1 }}>
                 {myAray.map((item, index) => <Song key={index} counter={index} />)}
-
             </Box>
         </Box>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { Box, Avatar } from '@mui/material';
 import mainBG from '@/app/assets/main-bg.jpg'
 import Image from 'next/image';
-import Grid from '@mui/material/Unstable_Grid2'
+import Grid from '@mui/material/Unstable_Grid2';
 import Profile from '@/app/components/main/sideBar/Profile';
 import Albums from '@/app/components/main/sideBar/Albums';
 import CurrentAlbum from './components/main/currentAlbum/CurrentAlbum';
 import Mix from './components/main/mix/Mix';
+import Action from './components/main/action/Action';
 const page = () => {
   return (
-    <Box sx={{ position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100dvh' }}>
+    <Box sx={{ position: 'relative', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100dvh' }}>
       <Avatar variant='rounded' sx={{ height: 1, width: 1, borderRadius: 0, position: 'absolute', top: 0, zIndex: -1 }}>
         <Image src={mainBG} alt='background picture' style={{ objectFit: 'cover', filter: 'brightness(90%) blur(2px)' }} fill />
       </Avatar>
-      <Box sx={{ height: 600, width: 1200, borderRadius: 10, p: 3 }} className='blur-layer'>
-        <Grid container rowSpacing={1} height={1} columnSpacing={2}>
+      <Box sx={{ height: 600, width: 1200, borderTopRightRadius: 40, borderTopLeftRadius: 40 }} className='blur-layer'>
+        <Grid container rowSpacing={1} height={1} columnSpacing={2} sx={{ p: 3 }}>
           <Grid xs={3} sx={{ display: 'flex', flexDirection: 'column' }}>
 
             <Profile />
@@ -24,13 +25,17 @@ const page = () => {
 
           <Grid xs={6}>
             <CurrentAlbum />
-          </Grid> 
+          </Grid>
 
           <Grid xs={3} sx={{ overflow: 'auto', height: 1 }}>
             <Mix />
           </Grid>
 
         </Grid>
+
+      </Box>
+      <Box sx={{ height: 70, width: 1200, borderBottomRightRadius: 40, borderBottomLeftRadius: 40, }} className='blur-layer-action'>
+        <Action />
       </Box>
     </Box>
   );
