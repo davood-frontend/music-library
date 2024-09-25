2bc223867ad841debc0226a346046bbcf640bd6d
David <davood.frontend@gmail.com>
2024-09-25 23:08:16 +0330

    feat(currentAlbum): add fake share and download feature

--- app/components/main/currentAlbum/Song.tsx
+++ app/components/main/currentAlbum/Song.tsx
@@ -1,9 +1,8 @@
 'use client'
 import React, { useRef } from 'react';
-import { Box, IconButton, Typography, Button, Tabs, Tab, Avatar } from '@mui/material';
+import { Box, IconButton, Typography, Avatar, Menu, MenuItem } from '@mui/material';
 import { TouchRippleProps, TouchRippleActions } from '@mui/material/ButtonBase/TouchRipple';
 import Image from 'next/image';
-import cover from '@/app/assets/albums/song-cover.jpg'
 import Grid from '@mui/material/Unstable_Grid2'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
@@ -16,13 +15,29 @@ const Song = ({ counter, data }: { counter: number, data: music_type }) => {
 
     //checks to see if the click was on the song itself or on a button on the song
     const handleMouseDown = (event: any) => {
+
+        console.log(event.target.tagName)
+        if (event.target.tagName === 'svg') {
+            return
+        }
+        if (event.target.tagName === 'BUTTON') {
+            return
+        }
+        if (event.target.tagName === 'path') {
+            return
+        }
+        if (event.target.tagName === 'LI') {
+            return
+        }
+        if (event.target.className.includes('MuiButtonBase-root')) {
+            return
+        }
+        if (event.target.className.includes('MuiBackdrop-root')) {
+            return
+        }
         if (rippleRef.current) {
-            if (!event.target.className.baseVal) {
-                rippleRef.current.start(event);
-                setCurrentSong(data)
-
-
-            }
+            rippleRef.current.start(event);
+            setCurrentSong(data)
         }
     };
 
@@ -38,22 +53,34 @@ const Song = ({ counter, data }: { counter: number, data: music_type }) => {
         let seconds = number - (minutes * 60)
         return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
     }
-    const nameFormatter = (data: String, type?: string) => {
+    const nameFormatter = (data: String) => {
         let newString = data.slice(0, 8)
         if (data.length > 8) {
             newString += '...'
         }
         return newString
     }
-    const songNameFormatter = (data: String, type?: string) => {
+    const songNameFormatter = (data: String) => {
         let newString = data.slice(0, 16)
         if (data.length > 16) {
             newString += '...'
         }
         return newString
     }
+
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.stopPropagation()
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = (event: MouseEvent) => {
+        event.stopPropagation()
+        setAnchorEl(null);
+    };
+
     return (
-        <Box sx={{ width: 1, display: 'flex', justifyContent: 'start', alignItems: 'center', borderRadius: 0.8, p: 1, overflow: 'hidden', cursor: 'pointer', position: 'relative', }} onMouseDown={handleMouseDown} >
+        <Box sx={{ width: 1, display: 'flex', justifyContent: 'start', alignItems: 'center', borderRadius: 0.8, p: 1, overflow: 'hidden', cursor: 'pointer', position: 'relative', }} onMouseDown={handleMouseDown}>
             <TouchRipple ref={rippleRef} />
             <Grid container sx={{ alignItems: 'center', width: 1 }}>
                 <Grid xs={1}>
@@ -81,9 +108,22 @@ const Song = ({ counter, data }: { counter: number, data: music_type }) => {
                     <Typography variant='subtitle2'>{numberFormatter(data.duration)}</Typography>
                 </Grid>
                 <Grid xs={1}>
-                    <IconButton size='small' >
+                    <IconButton size='small'
+                        id="basic-button"
+                        onClick={handleClick}
+                    >
                         <MoreHorizIcon className='inButton-icon' />
                     </IconButton>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+
+                    >
+                        <MenuItem>Share</MenuItem>
+                        <MenuItem>Download</MenuItem>
+                    </Menu>
                 </Grid>
             </Grid>
         </Box>
